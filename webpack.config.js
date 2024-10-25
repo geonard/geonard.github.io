cede64a7a442d73cda5046087482113e2141cd14
LAMBERT81 <georges.lambert18@gmail.com>
2024-10-25 15:50:00 +0200

    Ajout du code HTML et CSS pour le site de location de véhicules

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,30 @@
-const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build/js'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
+    entry: './src/index.js', // Changez le chemin si nécessaire
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/dist',
+        publicPath: '/'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html' // Changez le chemin si nécessaire
+        })
     ],
-  },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
 };
